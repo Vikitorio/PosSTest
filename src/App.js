4dcae8ca02edc7a57f1e54764920f9e166b35db9
Viktor M <mikhalcheskiu@gmail.com>
2024-01-26 21:24:35 +0200

    Basic App

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function fetchRequestTest(){
+    fetch('http://localhost/demo/',{
+      method: 'POST',
+      headers :{
+        'Content-Type':'test'
+      },
+      body : JSON.stringify({action:1})
+    })
+    .then(response => response.text())
+    .then(response => {
+      console.log(response);
+    })
+  }
   return (
     <div className="App">
+      <button onClick = {fetchRequestTest}>button</button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
